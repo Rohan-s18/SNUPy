10481871c673a8bde539cb06aa3f839a4bbd1b9f
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-02 22:16:02 -0800

    Added Hidden Layer NN Setup

---
+++ src/Neural_Networks/HiddenLayerNN.py
@@ -0,0 +1,11 @@
+"""
+Author: Rohan Singh
+1/2/2023
+This Python module contains the source code for a Neural Network with a Hidden Layer with a sigmoid non-linearity
+"""
+
+#Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
